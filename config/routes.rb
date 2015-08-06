1bf68422b9fdd50483810558578d2b28dd3fec56
Katherine Santiago <santiago.kat@gmail.com>
2015-08-06 18:33:14 -0400

    created salesforce developer job description

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ LenditRails::Application.routes.draw do
   # root 'conferences#china15'
 
   get '/home' => 'home#landing'
-  # get '/test' => 'agendas#eu15'
+  get '/test' => 'agendas#eu15'
 
   # get '/conferences' => 'welcome#home'
   get '/about' => 'welcome#about'
@@ -32,7 +32,10 @@ LenditRails::Application.routes.draw do
   get '/agenda' => 'agendas#usa15'
   get '/team' => 'welcome#team'
   get '/networking' => 'welcome#networking'
+
+
   get '/jobs' => 'jobs#index'
+  get '/jobs/salesforce-developer' => 'jobs#salesforce_developer'
   get '/jobs/vp-finance' => 'jobs#vp_finance'
   get '/jobs/speaker-manager' => 'jobs#content_manager'
   get '/jobs/content-manager', to: redirect('/jobs/speaker-manager', status: 301)
