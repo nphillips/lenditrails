7129c49327a45844bffe9abf701f3877066344fb
Katherine Santiago <santiago.kat@gmail.com>
2015-09-22 16:05:11 -0400

    added registration down page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ LenditRails::Application.routes.draw do
   root 'welcome#home'
   # root 'conferences#china15'
 
-   get '/home' => 'home#landing'
+  # get '/home' => 'home#landing'
+  get '/register' => 'welcome#regdown'
 
   # get '/conferences' => 'welcome#home'
   get '/about' => 'welcome#about'
